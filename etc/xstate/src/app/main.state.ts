de0063d410efdede92457b4e09734618d9a5c858
May_cho <chohyebinn@naver.com>
2024-01-18 14:12:39 +0900

    Study xstate delete logic

--- etc/xstate/src/app/main.state.ts
+++ etc/xstate/src/app/main.state.ts
@@ -18,7 +18,7 @@ export type ListContext = {
 };
 
 export const listMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBsCWsAuA6ARgezwGsBiAYQBkBJUgaQH0BZAeQDVKBROgFQEEAhANoAGALqJQABzyxUGVHgB24kAA9EAWgCMAFgDMWABwGArEICcmncYO7b2gDQgAnhv1mATAbPHTAdgOa7sYAvsGOaJi4BIRYqAqyxBCKYLEKAG5EKRHY+ESpsghxGQDGAIZyisIiVcpSMhVKSKqIuppCWELa2prenr4AbF4Gji4Iuu7uWP6+rf262gb9ZkLu2qHh6DnR+RiJyakZhFmbUXlxBUV4ZQ1VAppiTXWy8o2gagja-ljG-UJC1sYzLpfMYRohPFhxnoAsY2t1+v09OsQNksABbPBpVBgMhUWh0PhMJj0XiCUS1aTPRTKd79dy+Dr-AxdTSw4FmMFjdz6VZ-RFGTRzcahMIgBR4CBwZTZCn1F401z0yF6EGc9QTSb9EE2XwrIJmIbI1G5QiyqmvZoIdX9LAeMwWXQGTzA4z03xqzS6YxYTTa+a6ZZAoLuI0nE07M0NBUIdyaDq6oSLX2C1a+blqz5YLXWHrMnpe5YhUXG7Zxch4UqSiCR+VNd6+m3aISOxOAl2coIMp3Q1lwwVzUORcNxdhoiQYUaSSlRuuIBtYJstkxA1XOFoeSGrR29zr93SDrZnBQAMVKqGQkBr1NnCHni4MrZXoLXCFdNu729hu4RB-RmOxV4Wu86q+GYyrti+BgDAu1i2HB8H7iKQA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBsCWsAuA6ARgezwGsBiAYQBkBJUgaQH0BZAeQDVKBROgFQEEAhANoAGALqJQABzyxUGVHgB24kAA9EAWgCMAFgDMWABwGArEICcmncYO7b2gDQgAnhv1mATAbPHTAdgOa7sYAvsGOaJi4BIRYqAqyxBCKYLEKAG5EKRHY+ESpsghxGQDGAIZyisIiVcpSMhVKSKqIuppCWELa2prenr4AbF4Gji4Iuu7uWP6+rf262gb9ZkLu2qHh6DnR+RiJyakZhFmbUXlxBUV4ZQ1VAppiTXWy8o2gagja-ljG-UJC1sYzLpfMYRohPFhxnoAsY2t1+v09OsQNlTjE4uQ8KUIJBiAARdjkdhcTh8JhMGg1R7SZ6KZTvIRghBCZGo3LohSY7G49h4yhcOhkilUyQ0hr0xCM5yS1knAC2eDSqDAZCotEF5PovEEolqYpeEoQ-Xcvg6-wMXU0sOBZiZ430qz+iKMmjm41lkQVSpS512SQUPvSmSwqK9yp2hSD1xet111PqBqa7xN2iwHmtvjMS18bVt0rGxiw7it-QB2mNxgmun6oTCIAUeBx8Ca2T1CbpSdcJshehBTPU7nM33MVu0ZiBeh+NbrbOibdpr2aCHUC37gQM3w8C183QGJgMHq2Z3iGHn4s7CGLHV8f0Wmhzxs+7l0-c+WH6IICZgtPV0gP+h5oqkXI4hAZ6Jm8iD3v0WDaEIugGEIJhAn2+ZBKaBirAhVpwq6cyAeyqTsHKEgYKMortou7zQbB8GIchwKgvmugeJCWEwrhCK6AR2xxAAYqUqDIJA4EdpBCA0XBCFIYCjFMpWMGYdCOGdHhPF5DiwlyAoUCiVRkpMgimhscpsKqQigFhmAemGgOmY9nJ+bqJWRYWvMQSDIsO6aJZirhr6NkXgOrQOahoyWK6sHWLYMWxdxM7yn5gYgSJ8YLrZz7GfMjnhe4Zimt4NhxbFvnekRJFkYF4nBVlvZMeFeiTNo0XFTFpX+QoAlCalFHpUFmWhfVUFTmmLWtbWwRAA */
   id: "list",
   schema: {
     context: {} as ListContext,
@@ -48,9 +48,40 @@ export const listMachine = createMachine({
             onError: [],
           },
         },
-        inLoaded: {},
+        inLoaded: {
+          on: {
+            DELETE_BOOK: {
+              target: "deleting",
+            },
+            EDIT_BOOK: {},
+          },
+        },
         inEmpty: {},
         inFailed: {},
+        deleting: {
+          invoke: {
+            src: {
+              type: "delete_data",
+              data_type: "book",
+            },
+            onDone: [
+              {
+                target: "init",
+                cond: "isDeleteSuccess",
+              },
+              {
+                target: "isShowModal",
+              },
+            ],
+          },
+        },
+        isShowModal: {
+          on: {
+            CLICK_CHECK_BTN: {
+              target: "init",
+            },
+          },
+        },
       },
       on: {
         CLICK_MOVIE_TAB: "movie",
@@ -66,23 +97,47 @@ export const listMachine = createMachine({
               type: "load_data",
               data_type: "movie",
             },
-            onDone: [
-              {
-                target: "inEmpty",
-                cond: "isMovieEmpty",
-                actions: "update_movies",
-              },
-              {
-                target: "inLoaded",
-                actions: "update_movies",
-              },
-            ],
+            onDone: {
+              target: "inLoaded",
+              actions: "update_movies",
+            },
             onError: [],
           },
         },
-        inLoaded: {},
+        inLoaded: {
+          on: {
+            DELETE_MOVIE: {
+              target: "deleting",
+            },
+            EDIT_BOOK: {},
+          },
+        },
         inEmpty: {},
         inFailed: {},
+        deleting: {
+          invoke: {
+            src: {
+              type: "delete_data",
+              data_type: "movie",
+            },
+            onDone: [
+              {
+                target: "init",
+                cond: "isDeleteSuccess",
+              },
+              {
+                target: "isShowModal",
+              },
+            ],
+          },
+        },
+        isShowModal: {
+          on: {
+            CLICK_CHECK_BTN: {
+              target: "init",
+            },
+          },
+        },
       },
       on: {
         CLICK_BOOK_TAB: "book",
--- etc/xstate/src/app/page.tsx
+++ etc/xstate/src/app/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import { useMachine } from "@xstate/react";
 import { listMachine } from "./main.state";
 import { assign } from "xstate";
@@ -16,6 +16,15 @@ export default function Home() {
         );
         const data = await res.json();
         return data;
+      },
+      delete_data: async (context, event, { src }) => {
+        const res = await fetch(
+          `https://6447c01c7bb84f5a3e47bbb0.mockapi.io/${src.data_type}/${event.id}`,
+          {
+            method: "Delete",
+          }
+        );
+        return res.status === 200;
       },
     },
     actions: {
@@ -37,37 +46,80 @@ export default function Home() {
       isMovieEmpty: (context, event) => {
         return event.data.length === 0;
       },
+      isDeleteSuccess: (context, event) => {
+        return event.data;
+      },
     },
   });
 
   return (
     <main className={styles.main}>
       <div className={styles.tabContainer}>
-        <button onClick={() => send("CLICK_BOOK_TAB")}>책</button>
-        <button onClick={() => send("CLICK_MOVIE_TAB")}>영화</button>
+        <button
+          onClick={() => send("CLICK_BOOK_TAB")}
+          className={state.matches("book") ? styles.selected : ""}>
+          책
+        </button>
+        <button
+          onClick={() => send("CLICK_MOVIE_TAB")}
+          className={state.matches("movie") ? styles.selected : ""}>
+          영화
+        </button>
       </div>
-      {state.matches("book") && (
-        <div>
-          <h2>책</h2>
-          {state.matches("book.init") && <h3>책 불러오는중...</h3>}
-          {state.matches("book.inEmpty") && <h3>책이 없습니다</h3>}
-          {state.matches("book.inLoaded") &&
-            state.context.books.map((book, idx) => {
-              return <p>{book.book_title}</p>;
-            })}
-        </div>
-      )}
-      {state.matches("movie") && (
-        <div>
-          <h2>영화</h2>
-          {state.matches("movie.init") && <h3>영화 불러오는중...</h3>}
-          {state.matches("movie.inEmpty") && <h3>영화가 없습니다</h3>}
-          {state.matches("movie.inLoaded") &&
-            state.context.movies.map((movie, idx) => {
-              return <p>{movie.movie_title}</p>;
-            })}
-        </div>
-      )}
+      <div className={styles.contentContainer}>
+        {state.matches("book") && (
+          <>
+            <h2>책</h2>
+            {state.matches("book.init") && <h3>책 불러오는중...</h3>}
+            {state.matches("book.inEmpty") && <h3>책이 없습니다</h3>}
+            {state.matches("book.inLoaded") &&
+              state.context.books.map((book, idx) => {
+                return (
+                  <div className={styles.content}>
+                    {book.book_title}
+                    <button
+                      onClick={() => send("DELETE_BOOK", { id: book.id })}>
+                      삭제
+                    </button>
+                  </div>
+                );
+              })}
+            {state.matches("book.isShowModal") && (
+              <div className={styles.modal}>
+                <h4>삭제 실패</h4>
+                <p>삭제를 실패했습니다</p>
+                <button onClick={() => send("CLICK_CHECK_BTN")}>확인</button>
+              </div>
+            )}
+          </>
+        )}
+        {state.matches("movie") && (
+          <>
+            <h2>영화</h2>
+            {state.matches("movie.init") && <h3>영화 불러오는중...</h3>}
+            {state.matches("movie.inEmpty") && <h3>영화가 없습니다</h3>}
+            {state.matches("movie.inLoaded") &&
+              state.context.movies.map((movie, idx) => {
+                return (
+                  <div className={styles.content}>
+                    {movie.movie_title}
+                    <button
+                      onClick={() => send("DELETE_MOVIE", { id: movie.id })}>
+                      삭제
+                    </button>
+                  </div>
+                );
+              })}
+            {state.matches("movie.isShowModal") && (
+              <div className={styles.modal}>
+                <h4>삭제 실패</h4>
+                <p>삭제를 실패했습니다</p>
+                <button onClick={() => send("CLICK_CHECK_BTN")}>확인</button>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </main>
   );
 }
