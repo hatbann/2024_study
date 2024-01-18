cb38a06f33f57c71ef0cf9e1044241e374a81016
May_cho <chohyebinn@naver.com>
2024-01-18 11:39:00 +0900

    Study Xstate : Make tab, make diff fetch logic

--- etc/xstate/src/app/main.state.ts
+++ etc/xstate/src/app/main.state.ts
@@ -17,63 +17,76 @@ export type ListContext = {
   movies: MovieType[];
 };
 
-export const listMachine = createMachine(
-  {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBsCWsAuA6ARgezwGsBiAYQBkBJUgaQH0BZAeQDVKBROgFQEEAhANoAGALqJQABzyxUGVHgB24kAA9EAWgCMAFgDMWABwGArEICcmncYO7b2gDQgAnhv1mATAbPHTAdgOa7sYAvsGOaJhYALZ4AG6oYGRUtHR8TEz0vIKiylIycorKagjWBljGAGzagb4VQtUVjY4uCBXuWGa+5tq+7hWVQaFhIAp4EHDKERi50rLySkiqru6+WLp6vsbNGmZlFZsVOr7alSa7oeHo2PhEM-nzRYiBZdpCugZCZ7qb2whamlhNAdNLtdCDtF4zLoLiAprgCIQsKgFLI7nNCotikCKlhXu9PsYoT9nIhamt3HoAsZNPVNI1tDC4TdEcjyHgAIbjCBogoLUBYsl4j5fYktIKrAwU96aam0xrQ4ZMhFIhTsKISDAtSSzXmPBDY3FvYWE75bEkIXQecmUmU0hoVBWXSLMlUAMXZqGQkB5D0xT0FRoJRLNLWMfUMUqpdrpFUZV2icQSPox-I0KzMaw2IcQBkF1lsBcLCtCQA */
-    id: "list",
-    initial: "book",
-    states: {
-      book: {
-        initial: "init",
-        type: "parallel",
-        states: {
-          init: {
-            invoke: {
-              id: "load_books",
-              src: "load_books",
-              onDone: [{ target: "inLoaded", actions: "" }],
-              onError: [],
+export const listMachine = createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QBsCWsAuA6ARgezwGsBiAYQBkBJUgaQH0BZAeQDVKBROgFQEEAhANoAGALqJQABzyxUGVHgB24kAA9EAWgCMAFgDMWABwGArEICcmncYO7b2gDQgAnhv1mATAbPHTAdgOa7sYAvsGOaJi4BIRYqAqyxBCKYLEKAG5EKRHY+ESpsghxGQDGAIZyisIiVcpSMhVKSKqIuppCWELa2prenr4AbF4Gji4Iuu7uWP6+rf262gb9ZkLu2qHh6DnR+RiJyakZhFmbUXlxBUV4ZQ1VAppiTXWy8o2gagja-ljG-UJC1sYzLpfMYRohPFhxnoAsY2t1+v09OsQNksABbPBpVBgMhUWh0PhMJj0XiCUS1aTPRTKd79dy+Dr-AxdTSw4FmMFjdz6VZ-RFGTRzcahMIgBR4CBwZTZCn1F401z0yF6EGc9QTSb9EE2XwrIJmIbI1G5QiyqmvZoIdX9LAeMwWXQGTzA4z03xqzS6YxYTTa+a6ZZAoLuI0nE07M0NBUIdyaDq6oSLX2C1a+blqz5YLXWHrMnpe5YhUXG7Zxch4UqSiCR+VNd6+m3aISOxOAl2coIMp3Q1lwwVzUORcNxdhoiQYUaSSlRuuIBtYJstkxA1XOFoeSGrR29zr93SDrZnBQAMVKqGQkBr1NnCHni4MrZXoLXCFdNu729hu4RB-RmOxV4Wu86q+GYyrti+BgDAu1i2HB8H7iKQA */
+  id: "list",
+  schema: {
+    context: {} as ListContext,
+  },
+  initial: "book",
+  states: {
+    book: {
+      initial: "init",
+      states: {
+        init: {
+          invoke: {
+            src: {
+              type: "load_data",
+              data_type: "book",
             },
+            onDone: [
+              {
+                target: "inEmpty",
+                cond: "isBookEmpty",
+                actions: "update_books",
+              },
+              {
+                target: "inLoaded",
+                actions: "update_books",
+              },
+            ],
+            onError: [],
           },
-          inLoaded: {},
-          inEmpty: {},
-          inFailed: {},
         },
-        on: {
-          CLICK_MOVIE_TAB: "movie",
-        },
+        inLoaded: {},
+        inEmpty: {},
+        inFailed: {},
       },
+      on: {
+        CLICK_MOVIE_TAB: "movie",
+      },
+    },
 
-      movie: {
-        on: {
-          CLICK_BOOK_TAB: "book",
+    movie: {
+      initial: "init",
+      states: {
+        init: {
+          invoke: {
+            src: {
+              type: "load_data",
+              data_type: "movie",
+            },
+            onDone: [
+              {
+                target: "inEmpty",
+                cond: "isMovieEmpty",
+                actions: "update_movies",
+              },
+              {
+                target: "inLoaded",
+                actions: "update_movies",
+              },
+            ],
+            onError: [],
+          },
         },
+        inLoaded: {},
+        inEmpty: {},
+        inFailed: {},
+      },
+      on: {
+        CLICK_BOOK_TAB: "book",
       },
     },
   },
-  {
-    services: {
-      load_books: async () => {
-        const res = await fetch(
-          "https://6447c01c7bb84f5a3e47bbb0.mockapi.io/book"
-        );
-        const books = await res.json();
-        return books;
-      },
-    },
-    actions: {
-      update_books: assign({
-        books: (context, event) => {
-          console.log(event);
-          return [];
-        },
-      }),
-    },
-    guards: {
-      isBookEmpty: (context, event) => {
-        console.log(event);
-        return true;
-      },
-    },
-  }
-);
+});
--- etc/xstate/src/app/page.tsx
+++ etc/xstate/src/app/page.tsx
@@ -5,16 +5,69 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useMachine } from "@xstate/react";
 import { listMachine } from "./main.state";
+import { assign } from "xstate";
 
 export default function Home() {
-  const service = useMachine(listMachine);
+  const [state, send, service] = useMachine(listMachine, {
+    services: {
+      load_data: async (context, event, { src }) => {
+        const res = await fetch(
+          `https://6447c01c7bb84f5a3e47bbb0.mockapi.io/${src.data_type}`
+        );
+        const data = await res.json();
+        return data;
+      },
+    },
+    actions: {
+      update_books: assign({
+        books: (context, event) => {
+          return event.data;
+        },
+      }),
+      update_movies: assign({
+        movies: (context, event) => {
+          return event.data;
+        },
+      }),
+    },
+    guards: {
+      isBookEmpty: (context, event) => {
+        return event.data.length === 0;
+      },
+      isMovieEmpty: (context, event) => {
+        return event.data.length === 0;
+      },
+    },
+  });
 
   return (
     <main className={styles.main}>
       <div className={styles.tabContainer}>
-        <button>책</button>
-        <button>영화</button>
+        <button onClick={() => send("CLICK_BOOK_TAB")}>책</button>
+        <button onClick={() => send("CLICK_MOVIE_TAB")}>영화</button>
       </div>
+      {state.matches("book") && (
+        <div>
+          <h2>책</h2>
+          {state.matches("book.init") && <h3>책 불러오는중...</h3>}
+          {state.matches("book.inEmpty") && <h3>책이 없습니다</h3>}
+          {state.matches("book.inLoaded") &&
+            state.context.books.map((book, idx) => {
+              return <p>{book.book_title}</p>;
+            })}
+        </div>
+      )}
+      {state.matches("movie") && (
+        <div>
+          <h2>영화</h2>
+          {state.matches("movie.init") && <h3>영화 불러오는중...</h3>}
+          {state.matches("movie.inEmpty") && <h3>영화가 없습니다</h3>}
+          {state.matches("movie.inLoaded") &&
+            state.context.movies.map((movie, idx) => {
+              return <p>{movie.movie_title}</p>;
+            })}
+        </div>
+      )}
     </main>
   );
 }
