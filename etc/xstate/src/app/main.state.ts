63c8511548c79db6dad58956c8245ede75e63fd8
hatbann <0urloveis_great@naver.com>
2024-01-19 16:46:49 +0900

    Add xstate edit  button field

--- etc/xstate/src/app/main.state.ts
+++ etc/xstate/src/app/main.state.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { assign, createMachine } from "xstate";
+import { assign, createMachine } from 'xstate';
 
 export type BookType = {
   id: number;
@@ -19,30 +19,30 @@ export type ListContext = {
 
 export const listMachine = createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QBsCWsAuA6ARgezwGsBiAYQBkBJUgaQH0BZAeQDVKBROgFQEEAhANoAGALqJQABzyxUGVHgB24kAA9EAWgCMAFgDMWABwGArEICcmncYO7b2gDQgAnhv1mATAbPHTAdgOa7sYAvsGOaJi4BIRYqAqyxBCKYLEKAG5EKRHY+ESpsghxGQDGAIZyisIiVcpSMhVKSKqIuppCWELa2prenr4AbF4Gji4Iuu7uWP6+rf262gb9ZkLu2qHh6DnR+RiJyakZhFmbUXlxBUV4ZQ1VAppiTXWy8o2gagja-ljG-UJC1sYzLpfMYRohPFhxnoAsY2t1+v09OsQNlTjE4uQ8KUIJBiAARdjkdhcTh8JhMGg1R7SZ6KZTvIRghBCZGo3LohSY7G49h4yhcOhkilUyQ0hr0xCM5yS1knAC2eDSqDAZCotEF5PovEEolqYpeEoQ-Xcvg6-wMXU0sOBZiZ430qz+iKMmjm41lkQVSpS512SQUPvSmSwqK9yp2hSD1xet111PqBqa7xN2iwHmtvjMS18bVt0rGxiw7it-QB2mNxgmun6oTCIAUeBx8Ca2T1CbpSdcJshehBTPU7nM33MVu0ZiBeh+NbrbOibdpr2aCHUC37gQM3w8C183QGJgMHq2Z3iGHn4s7CGLHV8f0Wmhzxs+7l0-c+WH6IICZgtPV0gP+h5oqkXI4hAZ6Jm8iD3v0WDaEIugGEIJhAn2+ZBKaBirAhVpwq6cyAeyqTsHKEgYKMortou7zQbB8GIchwKgvmugeJCWEwrhCK6AR2xxAAYqUqDIJA4EdpBCA0XBCFIYCjFMpWMGYdCOGdHhPF5DiwlyAoUCiVRkpMgimhscpsKqQigFhmAemGgOmY9nJ+bqJWRYWvMQSDIsO6aJZirhr6NkXgOrQOahoyWK6sHWLYMWxdxM7yn5gYgSJ8YLrZz7GfMjnhe4Zimt4NhxbFvnekRJFkYF4nBVlvZMeFeiTNo0XFTFpX+QoAlCalFHpUFmWhfVUFTmmLWtbWwRAA */
-  id: "list",
+  id: 'list',
   schema: {
     context: {} as ListContext,
   },
-  initial: "book",
+  initial: 'book',
   states: {
     book: {
-      initial: "init",
+      initial: 'init',
       states: {
         init: {
           invoke: {
             src: {
-              type: "load_data",
-              data_type: "book",
+              type: 'load_data',
+              data_type: 'book',
             },
             onDone: [
               {
-                target: "inEmpty",
-                cond: "isBookEmpty",
-                actions: "update_books",
+                target: 'inEmpty',
+                cond: 'isBookEmpty',
+                actions: 'update_books',
               },
               {
-                target: "inLoaded",
-                actions: "update_books",
+                target: 'inLoaded',
+                actions: 'update_books',
               },
             ],
             onError: [],
@@ -51,9 +51,11 @@ export const listMachine = createMachine({
         inLoaded: {
           on: {
             DELETE_BOOK: {
-              target: "deleting",
+              target: 'deleting',
             },
-            EDIT_BOOK: {},
+            EDIT_BOOK: {
+              target: 'editing',
+            },
           },
         },
         inEmpty: {},
@@ -61,45 +63,46 @@ export const listMachine = createMachine({
         deleting: {
           invoke: {
             src: {
-              type: "delete_data",
-              data_type: "book",
+              type: 'delete_data',
+              data_type: 'book',
             },
             onDone: [
               {
-                target: "init",
-                cond: "isDeleteSuccess",
+                target: 'init',
+                cond: 'isDeleteSuccess',
               },
               {
-                target: "isShowModal",
+                target: 'isShowModal',
               },
             ],
           },
         },
+        editing: {},
         isShowModal: {
           on: {
             CLICK_CHECK_BTN: {
-              target: "init",
+              target: 'init',
             },
           },
         },
       },
       on: {
-        CLICK_MOVIE_TAB: "movie",
+        CLICK_MOVIE_TAB: 'movie',
       },
     },
 
     movie: {
-      initial: "init",
+      initial: 'init',
       states: {
         init: {
           invoke: {
             src: {
-              type: "load_data",
-              data_type: "movie",
+              type: 'load_data',
+              data_type: 'movie',
             },
             onDone: {
-              target: "inLoaded",
-              actions: "update_movies",
+              target: 'inLoaded',
+              actions: 'update_movies',
             },
             onError: [],
           },
@@ -107,7 +110,7 @@ export const listMachine = createMachine({
         inLoaded: {
           on: {
             DELETE_MOVIE: {
-              target: "deleting",
+              target: 'deleting',
             },
             EDIT_BOOK: {},
           },
@@ -117,16 +120,16 @@ export const listMachine = createMachine({
         deleting: {
           invoke: {
             src: {
-              type: "delete_data",
-              data_type: "movie",
+              type: 'delete_data',
+              data_type: 'movie',
             },
             onDone: [
               {
-                target: "init",
-                cond: "isDeleteSuccess",
+                target: 'init',
+                cond: 'isDeleteSuccess',
               },
               {
-                target: "isShowModal",
+                target: 'isShowModal',
               },
             ],
           },
@@ -134,13 +137,13 @@ export const listMachine = createMachine({
         isShowModal: {
           on: {
             CLICK_CHECK_BTN: {
-              target: "init",
+              target: 'init',
             },
           },
         },
       },
       on: {
-        CLICK_BOOK_TAB: "book",
+        CLICK_BOOK_TAB: 'book',
       },
     },
   },
